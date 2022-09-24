03282979f2c44299134c6eefb2c0d56857b519a4
arjun <arjunbr103@gmail.com>
2022-09-24 17:26:14 +0530

    updated list, added dates

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 import time
+import requests
 browser = webdriver.Firefox()
 browser.get("https://fitgirl-repacks.site/all-my-repacks-a-z/")
 time.sleep(10)
 listofgames= list()
-browser.find_element(By.ID,"smart_push_smio_not_allow").click()
+#browser.find_element(By.ID,"smart_push_smio_not_allow").click()
 tpage=browser.find_element(By.CLASS_NAME,"lcp_paginator")
 tlipage=tpage.find_elements(By.TAG_NAME,"li")
 finalpage=int(tlipage[-2].text)
@@ -27,15 +29,28 @@ htmlstart="""
 <html>
  <body>
   <h1>List of Multiplayer Games </h1>
-   <ol>
+   <table>
 """
 htmlend="""
-   </ol>
+   </table>
   </body>
  </html
 """
 htmlfile.write(htmlstart)
+datetimelist = []
+newlistofgames = []
 for i in listofgames:
-	listring='<li><a href="' + i[1] + '" target="_blank"> '+i[0]+"</a></li>"
-	htmlfile.write(listring)
+    r = requests.get(i[1])
+    soup = BeautifulSoup(r.text,'html.parser')
+    datetime = soup.find_all('time',class_ = 'entry-date')[0]['datetime']
+    print(datetime)
+    datetimelist.append(datetime)
+    newlistofgames.append((datetime,i[0], i[1],soup.find_all('time',class_ = 'entry-date')[0].get_text()))
+newlistofgames.sort()
+count = 0
+for i in newlistofgames:
+    count=count+1
+    listring='<td><a href="'+i[2]+'" target="_blank"> '+i[1]+"</a></td><td>"+i[3]+"</td></tr>"
+    listring='<tr><td>' + str(count) + '</td>' + listring
+    htmlfile.write(listring)
 htmlfile.write(htmlend)
